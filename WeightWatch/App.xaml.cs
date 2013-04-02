d656b433aaaf792a10fef1843a15fb27319ef268
Jason <github@jasonpoon.ca>
2013-04-01 22:44:34 -0700

    Integrate with Localytics

--- WeightWatch/App.xaml.cs
+++ WeightWatch/App.xaml.cs
@@ -29,6 +29,9 @@ namespace WeightWatch
 
     public partial class App : Application
     {
+        public LocalyticsSession appSession;
+        private const string LocalyticsAppKey = "4eb899d240987cbdd9c94c5-f8f8459c-319f-11e1-a167-008545fe83d2";
+
         /// <summary>
         /// Provides easy access to the root frame of the Phone Application.
         /// </summary>
@@ -72,24 +75,32 @@ namespace WeightWatch
         // This code will not execute when the application is reactivated
         private void Application_Launching(object sender, LaunchingEventArgs e)
         {
+            appSession = new LocalyticsSession(LocalyticsAppKey);
+            appSession.open();
+            appSession.upload();
         }
 
         // Code to execute when the application is activated (brought to foreground)
         // This code will not execute when the application is first launched
         private void Application_Activated(object sender, ActivatedEventArgs e)
         {
+            appSession = new LocalyticsSession(LocalyticsAppKey);
+            appSession.open();
+            appSession.upload();
         }
 
         // Code to execute when the application is deactivated (sent to background)
         // This code will not execute when the application is closing
         private void Application_Deactivated(object sender, DeactivatedEventArgs e)
         {
+            appSession.close();
         }
 
         // Code to execute when the application is closing (eg, user hit Back)
         // This code will not execute when the application is deactivated
         private void Application_Closing(object sender, ClosingEventArgs e)
         {
+            appSession.close();
         }
 
         // Code to execute if a navigation fails
